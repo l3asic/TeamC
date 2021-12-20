8bb21ac6f7aefdcd809440ef35914ecc7dabf2f5
ChaMinHwan <hwany0422@naver.com>
2021-12-20 11:26:24 +0900

    and_test_acc_미완성

---
+++ 01.Middle/src/dto/GridDTO.java
@@ -0,0 +1,31 @@
+package dto;
+
+public class GridDTO {
+	private String title, content;
+	private int imgresid;
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public int getImgresid() {
+		return imgresid;
+	}
+
+	public void setImgresid(int imgresid) {
+		this.imgresid = imgresid;
+	}
+
+}

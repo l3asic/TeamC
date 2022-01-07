91bd68b8a2c34ddb4a61b595d6223f79bcfab054
ChaMinHwan <hwany0528@gmail.com>
2022-01-07 20:01:03 +0900

    로그인부분 액티비티 모양'만' 싹다 인텐트로 완성함 ㅎ;

---
+++ ChaMinHwan_KakaoStory/app/src/main/java/com/example/chaminhwan_kakaostory/fgm/fgm05_mypage.java
@@ -0,0 +1,21 @@
+package com.example.chaminhwan_kakaostory.fgm;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.chaminhwan_kakaostory.R;
+
+
+public class fgm05_mypage extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_fgm01_home, container, false);
+    }
+}

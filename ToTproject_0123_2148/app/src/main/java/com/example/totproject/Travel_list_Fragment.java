8e1f43d235cfd5fcc6b8fe6d6d772c3f07eb00bd
SeungKyoung <momogomting@naver.com>
2022-01-23 21:52:18 +0900

    20220123_2152

---
+++ ToTproject_0123_2148/app/src/main/java/com/example/totproject/Travel_list_Fragment.java
@@ -0,0 +1,20 @@
+package com.example.totproject;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Travel_list_Fragment extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_travel_list_, container, false);
+    }
+}

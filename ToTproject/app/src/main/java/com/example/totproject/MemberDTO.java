b3bcfe00a1d42cfc3f046c0e5409d8bb8fcfa446
ChaMinHwan <hwany0422@naver.com>
2022-01-23 15:08:06 +0900

    안드로이드 xml 1차 통합

---
+++ ToTproject/app/src/main/java/com/example/totproject/MemberDTO.java
@@ -0,0 +1,47 @@
+package com.example.totproject;
+
+public class MemberDTO  {
+   private String id, pw, name, tel;
+
+    public MemberDTO(String id, String pw, String name, String tel) {
+        this.id = id;
+        this.pw = pw;
+        this.name = name;
+        this.tel = tel;
+    }
+
+    public MemberDTO(String id, String pw) {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getPw() {
+        return pw;
+    }
+
+    public void setPw(String pw) {
+        this.pw = pw;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+}

306097953260cffe0523266d906e7e3501f5ae82
won_keun <jjukiy@gmail.com>
2022-01-24 16:57:39 +0900

    no message

---
+++ ToTproject/app/src/main/java/com/example/totproject/JoinActivity.java
@@ -0,0 +1,38 @@
+package com.example.totproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class JoinActivity extends AppCompatActivity {
+
+    EditText join_id, join_pw, join_pw_confirm, join_tel;
+    TextView join_next;
+    Button join_id_confirm;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_join);
+
+        join_id = findViewById(R.id.join_id);
+        join_id_confirm = findViewById(R.id.join_id_confirm);
+        join_pw = findViewById(R.id.join_pw);
+        join_pw_confirm = findViewById(R.id.join_pw_confirm);
+        join_next = findViewById(R.id.join_next);
+
+
+        join_next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(JoinActivity.this, TendencyActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}

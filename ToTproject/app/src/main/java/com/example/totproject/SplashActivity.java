31d1e1f007c8bd16a269be63bed05936c4fa3f3f
ChaMinHwan <hwany0528@gmail.com>
2022-01-25 19:05:12 +0900

    KSK ver

--- ToTproject/app/src/main/java/com/example/totproject/SplashActivity.java
+++ ToTproject/app/src/main/java/com/example/totproject/SplashActivity.java
@@ -6,14 +6,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.WindowManager;
-import android.widget.Toast;
 
 public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_00_splash);
+        setContentView(R.layout.activity_splash);
 
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
@@ -22,9 +21,7 @@ public class SplashActivity extends AppCompatActivity {
             @Override
             public void run() {
                 //딜레를 주고나서 run을 실행함.
-                Toast.makeText(SplashActivity.this, "여기서 로그인 여부를 판별해 다음 액티비티를 정해야합니다 !", Toast.LENGTH_SHORT).show();
-
-                Intent intent = new Intent(SplashActivity.this , LoginActivity.class);
+                Intent intent = new Intent(SplashActivity.this , MainActivity.class);
                 startActivity(intent);
                 finish();//뒤로가기 했을때 액티비티가 없게 현재 액티비를 종료시킴.
             }

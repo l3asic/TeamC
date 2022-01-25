3f6d5393995d8e5c342a1a23b7d6c14585473227
ChaMinHwan <hwany0528@gmail.com>
2022-01-25 18:12:24 +0900

    no message

--- ToTproject/app/src/main/java/com/example/totproject/TendencyActivity01.java
+++ ToTproject/app/src/main/java/com/example/totproject/TendencyActivity01.java
@@ -7,14 +7,14 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class TendencyActivity extends AppCompatActivity {
+public class TendencyActivity01 extends AppCompatActivity {
 
     Button tend_btn1_skip, tend_btn2_next;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_tendenacyctivity);
+        setContentView(R.layout.activity_tendency_activity_01);
 
         tend_btn1_skip = findViewById(R.id.tend_btn1_skip);
         tend_btn2_next = findViewById(R.id.tend_btn2_next);
@@ -22,7 +22,7 @@ public class TendencyActivity extends AppCompatActivity {
         tend_btn1_skip.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(TendencyActivity.this, MainActivity.class);
+                Intent intent = new Intent(TendencyActivity01.this, MainActivity.class);
                 startActivity(intent);
             }
         });
@@ -30,7 +30,7 @@ public class TendencyActivity extends AppCompatActivity {
         tend_btn2_next.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(TendencyActivity.this, Tendency2Activity.class);
+                Intent intent = new Intent(TendencyActivity01.this, TendencyActivity02.class);
                 startActivity(intent);
             }
         });

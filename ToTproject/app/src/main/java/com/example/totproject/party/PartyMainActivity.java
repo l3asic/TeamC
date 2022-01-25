6e6121e1af9d166e91e29e23afc2f977e1eaee46
ChaMinHwan <hwany0528@gmail.com>
2022-01-25 19:09:01 +0900

    no message

---
+++ ToTproject/app/src/main/java/com/example/totproject/party/PartyMainActivity.java
@@ -0,0 +1,16 @@
+package com.example.totproject.party;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.totproject.R;
+
+public class PartyMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_04_party_main);
+    }
+}

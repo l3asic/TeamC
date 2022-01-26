17cc9e2cf191268477407115d7a82ad54d56840d
ChaMinHwan <hwany0528@gmail.com>
2022-01-26 20:02:11 +0900

    집가서 안할듯 ;ㅅ; ver2

---
+++ ToTproject/app/src/main/java/com/example/totproject/TendencyActivity02.java
@@ -0,0 +1,14 @@
+package com.example.totproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TendencyActivity02 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tendency_activity_02);
+    }
+}

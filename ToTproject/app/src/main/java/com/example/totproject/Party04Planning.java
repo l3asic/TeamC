6869c19306cdb32c655987dd5581d4e8ed17e314
SeungKyoung <momogomting@naver.com>
2022-01-26 18:29:45 +0900

    0126_1830 main burger 메뉴에 margintop 추가 → mainActivity에 풀스크린처리 소스 없어도 될듯 theme 에 style 추가

---
+++ ToTproject/app/src/main/java/com/example/totproject/Party04Planning.java
@@ -0,0 +1,14 @@
+package com.example.totproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Party04Planning extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_party04_planning_03_plan);
+    }
+}
---
+++ ToTproject/app/src/main/java/com/example/totproject/party/Myparty_list_Fragment.java
@@ -0,0 +1,24 @@
+package com.example.totproject.party;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.totproject.R;
+
+
+public class Myparty_list_Fragment extends Fragment {
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_04_myparty_list_, container, false);
+    }
+}

79d9b0c9627183ca569409cd78d2f002cf86ad52
ChaMinHwan <hwany0422@naver.com>
2022-01-30 19:31:33 +0900

    버거메뉴 좌<-우 이동 실패

--- ToTproject/app/src/main/java/com/example/totproject/main/MainActivity.java
+++ ToTproject/app/src/main/java/com/example/totproject/main/MainActivity.java
@@ -1,19 +1,30 @@
 package com.example.totproject.main;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.totproject.R;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
     BottomNavigationView bottom_nav;
     int container;
+
+    Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,22 +38,19 @@ public class MainActivity extends AppCompatActivity {
         Fragment04PartyTab partyTab_frag = new Fragment04PartyTab();
 
 
-
-
-
         bottom_nav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                if(item.getItemId() == R.id.bot_home){
+                if (item.getItemId() == R.id.bot_home) {
                     ChangeFrament(container, mainTab_frag);
                     return true;
-                }else if(item.getItemId() == R.id.bot_category){
+                } else if (item.getItemId() == R.id.bot_category) {
                     ChangeFrament(container, categoryTab_frag);
                     return true;
-                }else if(item.getItemId() == R.id.bot_board) {
+                } else if (item.getItemId() == R.id.bot_board) {
                     ChangeFrament(container, boardTab_frag);
                     return true;
-                }else if(item.getItemId() == R.id.bot_party) {
+                } else if (item.getItemId() == R.id.bot_party) {
                     ChangeFrament(container, partyTab_frag);
                     return true;
                 }
@@ -55,11 +63,32 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
+        toolbar = findViewById(R.id.main_toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+
+      ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar,
+                R.string.navigation_drawer_open,
+                R.string.navigation_drawer_close
+        );//햄버거 버튼 만들기 ( 버튼을 눌러서 반전시키는 효과를 만듬 )
+
+             drawer.addDrawerListener(toggle);
+               toggle.syncState();
+
+
+
+
+        NavigationView nav_view = findViewById(R.id.main_nav_view);
+        View nav_headerview = nav_view.getHeaderView(0);
+        ImageView nav_img = nav_headerview.findViewById(R.id.mainnav_image);
+        TextView nav_textv = nav_headerview.findViewById(R.id.mainnav_nickname);
 
 
     }//onCreate()
 
-    public void ChangeFrament(int container, Fragment fragment){
+    public void ChangeFrament(int container, Fragment fragment) {
         getSupportFragmentManager().beginTransaction().replace(container, fragment).commit();
     }
 

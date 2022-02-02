7f1e1dc156598d40140ddb9227b594ffeab39f28
SeungKyoung <a01056125925@gmail.com>
2022-02-03 00:38:12 +0900

    0203_1236 추가내용 탭메뉴에서 상세내용페이지로 화면전환 sn 값 보내기 추가 상세화면 리사이클러뷰 추가

---
+++ ToTproject_ccc/app/src/main/java/com/example/totproject/login/TendencyActivity02.java
@@ -0,0 +1,16 @@
+package com.example.totproject.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.totproject.R;
+
+public class TendencyActivity02 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tendency_activity_02);
+    }
+}

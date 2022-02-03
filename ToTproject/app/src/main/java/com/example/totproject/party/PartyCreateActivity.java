e942afe966186914f14589e81e61f2dd3dc5176f
ChaMinHwan <hwany0528@gmail.com>
2022-02-03 15:24:23 +0900

    no message

---
+++ ToTproject/app/src/main/java/com/example/totproject/party/PartyCreateActivity.java
@@ -0,0 +1,30 @@
+package com.example.totproject.party;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.example.totproject.R;
+
+public class PartyCreateActivity extends Activity {
+
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.party_act_createparty);
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ ToTproject/app/src/main/java/com/example/totproject/party_plan/Party04Planning.java
@@ -0,0 +1,16 @@
+package com.example.totproject.party_plan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.totproject.R;
+
+public class Party04Planning extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.partyplan_act_createplan02);
+    }
+}

ff3d58a07e63e61d9a6ffd70f05fc38864d6c73d
ChaMinHwan <hwany0528@gmail.com>
2022-02-18 14:02:41 +0900

    no message

---
+++ ToTproject/app/src/main/java/com/example/totproject/board/BoardNewActivity.java
@@ -0,0 +1,16 @@
+package com.example.totproject.board;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.totproject.R;
+
+public class BoardNewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.board_act_new);
+    }
+}

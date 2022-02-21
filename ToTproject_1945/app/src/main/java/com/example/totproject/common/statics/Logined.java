4541ff1ed33ff8cacaeb666c48f39c0dce5f365f
SeungKyoung <a01056125925@gmail.com>
2022-02-21 14:20:05 +0900

    0221_1420_댓글삭제기능 추가본

---
+++ ToTproject_1945/app/src/main/java/com/example/totproject/common/statics/Logined.java
@@ -0,0 +1,26 @@
+package com.example.totproject.common.statics;
+
+import android.content.SharedPreferences;
+
+import androidx.annotation.Nullable;
+
+import java.io.Serializable;
+import java.util.Map;
+import java.util.Set;
+
+public class Logined implements Serializable {
+    public static boolean isLogined = false;
+    public static String member_id = "1111";
+    public static String member_pw;
+    public static String member_name;
+    public static String member_nick;
+    public static String member_gender;
+    public static String member_tel;
+    public static String member_email;
+    public static String member_grade;
+    public static String member_is_kakao;
+    public static String member_is_naver;
+
+
+
+}
